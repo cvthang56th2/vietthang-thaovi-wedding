bbcc0ced6586757b1f36422aa23d491979659142
ThangCao <cvthang56th2@gmail.com>
2024-09-18 02:57:52 +0700

    up  config

--- js/script.js
+++ js/script.js
@@ -40,17 +40,6 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBXz2OUOrJ66a5wI2Y1TDEfaEPMkYxu8Uc",
-  authDomain: "vietthang-thaovi-wedding.firebaseapp.com",
-  projectId: "vietthang-thaovi-wedding",
-  storageBucket: "vietthang-thaovi-wedding.appspot.com",
-  messagingSenderId: "724246762373",
-  appId: "1:724246762373:web:e3a0a5afd76b9e7962aaec",
-  measurementId: "G-4BRJP4F7M2"
-};
-
 let listBless = [];
 
 // Initialize Firebase
